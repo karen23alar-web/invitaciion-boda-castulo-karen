002621b32b51627da28c811ca46fa0c9784469ba
karen23alar-web <karen.23alar@gmail.com>
2026-07-25 15:52:01 -0600

    Actualización del script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const sobre = document.getElementById("sobreImg");
 
+sobre.addEventListener("click",()=>{
+
+sobre.style.transform="scale(1.08)";
+sobre.style.opacity="0";
+
+setTimeout(()=>{
+window.location.href="noscasamos.html";
+},800);
+
+});
